df0bd11c8f0f4ebf25041af223cfa620c0449c7d
C-Elegans <mtnolan2640@gmail.com>
2016-05-24 16:16:25 -0400

    Fix my spacing (sorry).

--- errors.cpp
+++ errors.cpp
@@ -221,15 +221,15 @@ void AddSPASMErrorSessionAnnotation(int nSession, LPCTSTR lpszFormat, ...)
 	}
 }
 void EndSPASMErrorSession(int fatalErrors){
-    if(fatalErrors >0){
-        exit(1);
-    }
+	if(fatalErrors >0){
+		exit(1);
+	}
 
 }
 //returns the number of fatal errors
 int CleanupSPASMErrorSession(int nSession)
 {
-    int fatalErrorCount = 0;
+	int fatalErrorCount = 0;
 	list_t *pList = (list_t *) g_ErrorList;
 	
 	list_t *pPrev = NULL, *old_list = NULL;
@@ -245,9 +245,9 @@ int CleanupSPASMErrorSession(int nSession)
 		{
 			pPrev->next = pList->next;
 		}
-        if(IsSPASMErrorFatal(lpErr->dwErrorCode)){
-            fatalErrorCount++;
-        }
+		if(IsSPASMErrorFatal(lpErr->dwErrorCode)){
+			fatalErrorCount++;
+		}
 		FreeErrorInstance(lpErr);
 		list_t *pListOld = pList;
 		pList = pList->next;
